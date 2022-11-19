df0ccb35c363887ce7328403e7d07cc2cfb577be
Marcel-Thiele <me@marcel-thiele.de>
2022-11-19 11:18:26 +0100

    cleanup Flugsimulation

--- marcel/uebungsblatt2/Flugsimulation.java
+++ marcel/uebungsblatt2/Flugsimulation.java
@@ -3,28 +3,29 @@ package marcel.uebungsblatt2;
 public class Flugsimulation {
 	public static void main(String[] args) {
 		double fluggastErscheintWahrscheinlichkeit = 0.92;
-		
+
 		int flugplaetzeProFlugzeug = 75;
-		
+
 		int verkaufteFlugplaetzeProFlugzeug = 78;
-		
+
 		int numOfSimulations = Integer.parseInt(args[0]);
-		
+
 		int numOfUeberbuchungen = 0;
 		int sumOfBuchungen = 0;
-		for(int i = 0; i < numOfSimulations; i++) {
+		for (int i = 0; i < numOfSimulations; i++) {
 			int anzahlPassagiereDieKommen = 0;
-			for(int j = 0; j < verkaufteFlugplaetzeProFlugzeug; j++) {
-				anzahlPassagiereDieKommen += Math.random() > fluggastErscheintWahrscheinlichkeit? 0 : 1;
+			for (int j = 0; j < verkaufteFlugplaetzeProFlugzeug; j++) {
+				anzahlPassagiereDieKommen += Math.random() > fluggastErscheintWahrscheinlichkeit ? 0 : 1;
 			}
-			if(anzahlPassagiereDieKommen > flugplaetzeProFlugzeug)
+			if (anzahlPassagiereDieKommen > flugplaetzeProFlugzeug)
 				numOfUeberbuchungen++;
 			sumOfBuchungen += anzahlPassagiereDieKommen;
 		}
-		
-		double percentageOfUeberbuchungen = ((double)numOfUeberbuchungen / (double)numOfSimulations) * 100;
-		double mittelWertBuchungen = ((double) sumOfBuchungen) /((double) numOfSimulations);
-		System.out.println("Überbuchungen: " + numOfUeberbuchungen + ", bei " + numOfSimulations + " Simulationen; (" + percentageOfUeberbuchungen + "%)");
-		System.out.println("Mittelwert: " + mittelWertBuchungen);
+
+		double percentageOfUeberbuchungen = ((double) numOfUeberbuchungen / (double) numOfSimulations) * 100;
+		double mittelWertBuchungen = ((double) sumOfBuchungen) / ((double) numOfSimulations);
+		System.out.println("Überbuchungen: " + numOfUeberbuchungen + " ("
+				+ String.format("%.2f", percentageOfUeberbuchungen) + "%)");
+		System.out.println("Mittelwert: " + String.format("%.1f", mittelWertBuchungen));
 	}
 }
