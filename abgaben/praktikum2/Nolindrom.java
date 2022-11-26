f8abbecc03c4ef911c9fbd0b25edf4d0552275bf
Marcel-Thiele <me@marcel-thiele.de>
2022-11-26 18:22:10 +0100

    set to true

--- abgaben/praktikum2/Nolindrom.java
+++ abgaben/praktikum2/Nolindrom.java
@@ -14,7 +14,7 @@ public class Nolindrom {
 
         if (args.length == 2) {
             if (args[1].equals("x")) {
-                getNolindroms(numToTest, false);
+                getNolindroms(numToTest, true);
             }
         } else {
             getNolindromsWithLong(numToTest);
