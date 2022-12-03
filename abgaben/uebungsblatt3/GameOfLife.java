f06c09b27f4005de3504d7d54a4e8ec05c7cee83
Marcel-Thiele <me@marcel-thiele.de>
2022-12-03 20:45:45 +0100

    added abgaben uebungsblatt 3

---
+++ abgaben/uebungsblatt3/GameOfLife.java
@@ -0,0 +1,119 @@
+import gdp.stdlib.StdDraw;
+import gdp.stdlib.StdIn;
+import gdp.stdlib.StdOut;
+
+public class GameOfLife{
+
+    public static void main(String[] args){
+        int x_length = StdIn.readInt();
+        int y_length = StdIn.readInt();
+        int numOfLivingCells = StdIn.readInt();
+
+        boolean[][] mapOfCells = initMapOfCells(x_length, y_length, numOfLivingCells);
+        initDraw(x_length, y_length);
+
+
+        while(true){
+            draw(mapOfCells);
+            mapOfCells = calculateNextStep(mapOfCells);
+        }
+    }
+
+    private static void initDraw(int x_length, int y_length) {
+        int length = x_length < y_length ? y_length : x_length;
+
+        StdDraw.setXscale(0, length);
+        StdDraw.setYscale(0, length);
+    }
+
+    private static boolean[][] initMapOfCells(int x_length, int y_length, int numOfLivingCells) {
+        StdOut.println("Initializing Map...");
+        boolean[][] mapOfCells = new boolean[x_length][y_length];
+        for(int i = 0; i < numOfLivingCells; i++){
+            int x_read = StdIn.readInt();
+            int y_read = StdIn.readInt();
+            int x_pos = wrapX(x_read, mapOfCells);
+            int y_pos = wrapY(y_read, mapOfCells);
+
+            mapOfCells[x_pos][y_pos] = true;
+        }
+        StdOut.println("DONE - map initialized!");
+        return mapOfCells;
+    }
+
+    private static boolean[][] calculateNextStep(boolean[][] mapOfCells) {
+        StdOut.println("Calculating next step");
+
+        int x_length = mapOfCells.length;
+        int y_length = mapOfCells[0].length;
+        boolean[][] newMapOfCells = new boolean[x_length][y_length];
+
+        for(int x = 0; x < x_length; x++){
+            for(int y = 0; y < y_length; y++){
+                int numOfNeighbours = getNumOfNeighbours(x, y, mapOfCells);
+                if(mapOfCells[x][y] == false && numOfNeighbours == 3) newMapOfCells[x][y] = true;
+                else if(mapOfCells[x][y] == true && (numOfNeighbours == 3 || numOfNeighbours == 2)) newMapOfCells[x][y] = true;
+                else newMapOfCells[x][y] = false;
+            }
+        }
+        return newMapOfCells;
+    }
+
+    private static void draw(boolean[][] mapOfCells) {
+        StdOut.println("drawing next iteration");
+        StdDraw.clear();
+
+        for(int x = 0; x < mapOfCells.length; x++){
+            for(int y = 0; y < mapOfCells[0].length; y++){
+                if(mapOfCells[x][y]) StdDraw.setPenColor(StdDraw.GREEN);
+                else if(!mapOfCells[x][y]) StdDraw.setPenColor(StdDraw.WHITE);
+                StdDraw.filledSquare(x, y, 1);
+            }
+        }
+
+        // //Draw Lines
+        StdDraw.setPenColor(StdDraw.BLUE);
+        for(int x = 0; x <= mapOfCells.length; x++){
+            //Vertical lines
+            StdDraw.line(x, 0, x, mapOfCells[0].length);
+        }
+        for(int y = 0; y <= mapOfCells[0].length; y++){
+            //Horizontal lines
+            StdDraw.line(0, y, mapOfCells.length, y);
+        }
+
+
+        StdDraw.show(100);
+    }
+
+    private static int getNumOfNeighbours(int x_pos, int y_pos, boolean[][] mapOfCells) {
+        int sumOfNeighbours = 0;
+        for(int x_delta = -1; x_delta <= 1; x_delta++){
+            for(int y_delta = -1; y_delta <= 1; y_delta++){
+                int x = wrapX(x_pos+x_delta, mapOfCells);
+                int y = wrapY(y_pos+y_delta, mapOfCells);
+                // int x = x_pos+x_delta;
+                // int y = y_pos+y_delta;
+
+                if(mapOfCells[x][y]) sumOfNeighbours++;
+            }
+        }
+
+        if(mapOfCells[x_pos][y_pos]) sumOfNeighbours--;
+
+        return sumOfNeighbours;
+    }
+
+    private static int wrapX(int x_pos, boolean[][] mapOfCells) {
+        if(x_pos < 0) return mapOfCells.length+x_pos;
+        if(x_pos >= mapOfCells.length) return x_pos - mapOfCells.length;
+        return x_pos;
+    }
+
+    private static int wrapY(int y_pos, boolean[][] mapOfCells) {
+        if(y_pos < 0) return mapOfCells[0].length+y_pos;
+        if(y_pos >= mapOfCells[0].length) return y_pos - mapOfCells[0].length;
+        return y_pos;
+    }
+    
+}
---
+++ abgaben/uebungsblatt3/Sieb.java
@@ -0,0 +1,54 @@
+public class Sieb {
+	public static boolean[] groesser(boolean[] p) {
+		boolean[] newp = new boolean[p.length * 10];
+		for (int j = 0; j < newp.length; j++) {
+			newp[j] = true;
+
+		}
+		for (int i = 2; i < p.length && i < newp.length; i++) {
+
+			newp[i] = p[i];
+		}
+
+		return newp;
+	}
+
+	public static void main(String[] args) {
+		int N = Integer.parseInt(args[0]);
+		boolean[] p = new boolean[9];
+		int anzahl = 0, i, j, n;
+
+		for (i = 2; i < p.length; i++) {
+			p[i] = true;
+		}
+
+		while (anzahl < N) {
+			for (i = 2; i < p.length; i++) {
+
+				if (p[i]) {
+					anzahl++;
+
+					for (n = 2; n * i < p.length; n++)
+						p[n * i] = false;
+				}
+			}
+			if (anzahl < N) {
+				p = groesser(p);
+			}
+		}
+		anzahl = 0;
+		for (i = 2; i < p.length; i++) {
+			if (p[i]) {
+				System.out.println(i);
+				anzahl++;
+				if (anzahl == N) {
+					System.exit(-1);
+				}
+
+			}
+
+		}
+
+	}
+
+}
