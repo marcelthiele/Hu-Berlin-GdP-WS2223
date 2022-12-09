c2077f3343bc2bd2c23b29c215e9745ba7295a98
Marcel-Thiele <me@marcel-thiele.de>
2022-12-09 20:42:17 +0100

    armstrong numbers should be donw

--- marcel/uebungsblatt4/ArmstrongNumbers.java
+++ marcel/uebungsblatt4/ArmstrongNumbers.java
@@ -2,13 +2,19 @@ package marcel.uebungsblatt4;
 
 public class ArmstrongNumbers {
     public static void main(String[] args) {
-        System.out.println(isArmstrongNumber(370));
+        try {
+            int inputNumber = Integer.parseInt(args[0]);
+            int[] numbers = giveArmstrongNumbers(inputNumber);
+            printArray(numbers);
+        } catch (Exception e) {
+            System.out.println("Bitte eine akzeptierte Zahl eingeben");
+            System.exit(1);
+        }
     }
 
     public static boolean isArmstrongNumber(int toCheck){
         int countOfNumbers = (int)Math.log10((double)toCheck)+1;
         int[] individualNumbers = new int[countOfNumbers]; 
-        System.out.println("countOfNumber: " + countOfNumbers);
 
         int tempForIndividualNumberConversion = toCheck;
         for(int i = 0; i < countOfNumbers; i++){
@@ -16,7 +22,6 @@ public class ArmstrongNumbers {
             tempForIndividualNumberConversion /= 10;
 
             individualNumbers[i] = currentNumber;
-            System.out.println(currentNumber);
         }
 
         int checkSum = 0;
@@ -28,6 +33,32 @@ public class ArmstrongNumbers {
     }
 
     public static int[] giveArmstrongNumbers(int numbersToReturn){
-        return new int[6];
+        if(numbersToReturn == 0) return new int[0];
+
+        int countOfFoundNumbers = 1;
+        int[] returnArray = new int[numbersToReturn];
+        returnArray[0] = 0;
+
+        int currentNumber = 1;
+        while(countOfFoundNumbers < numbersToReturn){
+            if(isArmstrongNumber(currentNumber)){
+                returnArray[countOfFoundNumbers] = currentNumber;
+                countOfFoundNumbers++;
+            }
+            currentNumber++;
+        }
+
+        return returnArray;
+    }
+
+    public static void printArray(int[] arrayToPrint){
+        String printString = "";
+        for(int i = 0; i < arrayToPrint.length; i++){
+            printString += arrayToPrint[i];
+            if(i < arrayToPrint.length-1)
+                printString += ",";
+        }
+
+        System.out.println(printString);
     }
 }
