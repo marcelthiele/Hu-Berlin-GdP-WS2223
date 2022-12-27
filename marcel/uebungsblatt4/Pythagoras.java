bb776e176cd16c0ce2b09b52ccdb17bb14d07e8e
Marcel-Thiele <me@marcel-thiele.de>
2022-12-27 17:38:12 +0100

    drawing next iteration works

--- marcel/uebungsblatt4/Pythagoras.java
+++ marcel/uebungsblatt4/Pythagoras.java
@@ -6,7 +6,7 @@ public class Pythagoras {
     public static void main(String[] args) {
         StdDraw.setXscale(0, 3);
         StdDraw.setYscale(0, 3);
-        printNextIteration(1, 1, 2, 1, 0 , 2);
+        printNextIteration(1, 1, 2, 1, 0, 2);
     }
 
     /**
@@ -17,21 +17,23 @@ public class Pythagoras {
      * @param prevX2
      * @param prevY2
      */
-    public static void printNextIteration(double prevX1, double prevY1, double prevX2, double prevY2, int currentLevel, int levelToStop) {
-        if(currentLevel >= levelToStop) return;
-        
+    public static void printNextIteration(double prevX1, double prevY1, double prevX2, double prevY2, int currentLevel,
+            int levelToStop) {
+        if (currentLevel >= levelToStop)
+            return;
+
         double x_delta = prevX2 - prevX1;
         double y_delta = prevY2 - prevY1;
         double prevLineLength = Math.sqrt(Math.pow(x_delta, 2) + Math.pow(y_delta, 2));
 
         double prevAngle = 0;
         System.out.println("XDelta: " + x_delta);
-        if(x_delta > 0)
-            prevAngle = Math.PI/2 + Math.acos(Math.abs(x_delta)/prevLineLength);
+        if (x_delta > 0)
+            prevAngle = Math.PI / 2 + Math.acos(Math.abs(x_delta) / prevLineLength);
         else
-             prevAngle = Math.asin(Math.abs(x_delta) / prevLineLength);
+            prevAngle = Math.asin(Math.abs(x_delta) / prevLineLength);
 
-        //Calculate Square Coordinates
+        // Calculate Square Coordinates
         double squareLowerLeftX = prevX1;
         double squareLowerLeftY = prevY1;
         double squareLowerRightX = prevX2;
@@ -42,14 +44,19 @@ public class Pythagoras {
         double squareUpperRightX = prevX2 + Math.cos(prevAngle) * prevLineLength;
         double squareUpperRightY = prevY2 + Math.sin(prevAngle) * prevLineLength;
 
-        //Calculate Triangle Coordinates
+        // Calculate Triangle Coordinates
         double alpha = Math.random() * Math.PI / 6 + Math.PI / 6;
         System.out.println("Alpha: " + alpha);
 
         double aLength = Math.sin(alpha) * prevLineLength;
         double bLength = Math.cos(alpha) * prevLineLength;
 
-        double triangleTopX = squareUpperLeftX + Math.cos(alpha) * bLength;
+        double calcAngle = 0;
+        if (x_delta > 0)
+            calcAngle = alpha - prevAngle;
+        else
+            calcAngle = alpha + prevAngle;
+        double triangleTopX = squareUpperLeftX + Math.cos(calcAngle) * bLength;
         double triangleTopY = squareUpperLeftY + Math.sin(alpha) * bLength;
 
         // Draw Square
@@ -58,11 +65,13 @@ public class Pythagoras {
         StdDraw.line(squareLowerRightX, squareLowerRightY, squareLowerLeftX, squareLowerLeftY);
         StdDraw.line(squareLowerLeftX, squareLowerLeftY, squareUpperLeftX, squareUpperLeftY);
 
-        //Draw Triangle
+        // Draw Triangle
         StdDraw.line(squareUpperLeftX, squareUpperLeftY, triangleTopX, triangleTopY);
         StdDraw.line(squareUpperRightX, squareUpperRightY, triangleTopX, triangleTopY);
 
-        printNextIteration(squareUpperLeftX, squareUpperLeftY, triangleTopX, triangleTopY, currentLevel+1, levelToStop);
-        printNextIteration(squareUpperRightX, squareUpperRightY, triangleTopX, triangleTopY,  currentLevel+1, levelToStop);
+        printNextIteration(squareUpperLeftX, squareUpperLeftY, triangleTopX, triangleTopY, currentLevel + 1,
+                levelToStop);
+        printNextIteration(squareUpperRightX, squareUpperRightY, triangleTopX, triangleTopY, currentLevel + 1,
+                levelToStop);
     }
 }
