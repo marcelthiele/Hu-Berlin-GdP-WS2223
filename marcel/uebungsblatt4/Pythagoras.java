0d094862a8c4591bf5ce2865d0ef54a647b2e3fd
Marcel-Thiele <me@marcel-thiele.de>
2022-12-28 23:51:48 +0100

    Pythagoras Done

--- marcel/uebungsblatt4/Pythagoras.java
+++ marcel/uebungsblatt4/Pythagoras.java
@@ -4,145 +4,88 @@ import gdp.stdlib.StdDraw;
 
 public class Pythagoras {
     public static void main(String[] args) {
-        StdDraw.setXscale(0, 15);
-        StdDraw.setYscale(0, 15);
-        drawNextHouse(5, 5, 8, 5, 0, 1);
+        StdDraw.setCanvasSize(1000, 1000);
+
+        StdDraw.setXscale(0, 30);
+        StdDraw.setYscale(0, 30);
+
+        double x1 = 13;
+        double y1 = 2;
+        double x2 = 17;
+        double y2 = 2;
+        double x3 = 17;
+        double y3 = 6;
+        double x4 = 13;
+        double y4 = 6;
+
+        drawSquare(x1, y1, x2, y2, x3, y3, x4, y4);
+        draw(x4, y4, x3, y3, 0, 0, 10);
     }
 
-    /**
-     * Erstellt ein neues Qadrat und die dazugehörigen Seiten des Dreiecks
-     * 
-     * @param prevX1
-     * @param prevY1
-     * @param prevX2
-     * @param prevY2
-     */
-    public static void printNextIteration(double prevX1, double prevY1, double prevX2, double prevY2, int currentLevel,
+    public static void draw(double x1, double y1, double x2, double y2, double prevAlpha, int currentLevel,
             int levelToStop) {
         if (currentLevel >= levelToStop)
             return;
-        System.out.println("-------");
 
-        double x_delta = prevX2 - prevX1;
-        double y_delta = prevY2 - prevY1;
-        double prevLineLength = Math.sqrt(Math.pow(x_delta, 2) + Math.pow(y_delta, 2));
-
-        double prevAngle = 0;
-        System.out.println("XDelta: " + x_delta);
-        System.out.println("prevLineLength: " + prevLineLength);
-        if (x_delta > 0) {
-            double temp = Math.abs(x_delta) / prevLineLength;
-            System.out.println("temp: " + temp);
-            prevAngle = Math.PI / 2 + Math.acos(temp);
-            System.out.println("Prev Angle: " + (prevAngle - Math.PI / 2));
-        } else {
-            prevAngle = Math.asin(Math.abs(x_delta) / prevLineLength);
-            System.out.println("Prev Angle: " + (prevAngle));
-        }
-
-        // Calculate Square Coordinates
-        double squareLowerLeftX = prevX1;
-        double squareLowerLeftY = prevY1;
-        double squareLowerRightX = prevX2;
-        double squareLowerRightY = prevY2;
-
-        double squareUpperLeftX = prevX1 + Math.cos(prevAngle) * prevLineLength;
-        double squareUpperLeftY = prevY1 + Math.sin(prevAngle) * prevLineLength;
-        double squareUpperRightX = prevX2 + Math.cos(prevAngle) * prevLineLength;
-        double squareUpperRightY = prevY2 + Math.sin(prevAngle) * prevLineLength;
-
-        // Calculate Triangle Coordinates
         double alpha = Math.random() * Math.PI / 6 + Math.PI / 6;
-        System.out.println("Alpha: " + alpha);
 
-        double aLength = Math.sin(alpha) * prevLineLength;
-        double bLength = Math.cos(alpha) * prevLineLength;
+        double prevLineLength = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
 
-        double calcAngle = 0;
-        if (x_delta < 0)
-            calcAngle = alpha + prevAngle;
-        else
-            calcAngle = alpha - prevAngle;
+        double a = Math.sin(alpha) * prevLineLength;
+        double b = Math.cos(alpha) * prevLineLength;
 
-        double triangleTopX = squareUpperLeftX + Math.cos(calcAngle) * bLength;
-        double triangleTopY = squareUpperLeftY + Math.sin(alpha) * bLength;
+        double dx1 = Math.sin(Math.PI/2 - (alpha + prevAlpha));
+        double dy1 = Math.cos(Math.PI/2 - (alpha + prevAlpha));
 
-        // Draw Square
-        StdDraw.line(squareUpperLeftX, squareUpperLeftY, squareUpperRightX, squareUpperRightY);
-        StdDraw.line(squareUpperRightX, squareUpperRightY, squareLowerRightX, squareLowerRightY);
-        StdDraw.line(squareLowerRightX, squareLowerRightY, squareLowerLeftX, squareLowerLeftY);
-        StdDraw.line(squareLowerLeftX, squareLowerLeftY, squareUpperLeftX, squareUpperLeftY);
+        double x3 = x1 + b * dx1;
+        double y3 = y1 + b * dy1;
 
-        // Draw Triangle
-        StdDraw.line(squareUpperLeftX, squareUpperLeftY, triangleTopX, triangleTopY);
-        StdDraw.line(squareUpperRightX, squareUpperRightY, triangleTopX, triangleTopY);
+        drawTriangle(x1, y1, x2, y2, x3, y3);
 
-        StdDraw.show(2000);
+        double dx2 = Math.cos(Math.PI / 2 + (alpha + prevAlpha));
+        double dy2 = Math.sin(Math.PI / 2 + (alpha + prevAlpha));
 
-        printNextIteration(squareUpperLeftX, squareUpperLeftY, triangleTopX, triangleTopY, currentLevel + 1,
-                levelToStop);
-        printNextIteration(squareUpperRightX, squareUpperRightY, triangleTopX, triangleTopY, currentLevel + 1,
-                levelToStop);
+        double x4 = x3 + b * dx2;
+        double y4 = y3 + b * dy2;
+
+        double x5 = x1 + b * dx2;
+        double y5 = y1 + b * dy2;
+
+        drawSquare(x1, y1, x3, y3, x4, y4, x5, y5);
+        draw(x5, y5, x4, y4, prevAlpha + alpha, currentLevel + 1, levelToStop);
+
+        double dx3 = Math.sin(alpha + prevAlpha - Math.PI/2);
+        double dy3 = Math.cos(alpha + prevAlpha - Math.PI/2);
+
+        // System.out.println("dx3: " + dx3 + ", dy3: " + dy3);
+        // System.out.println("a: " + a + ", b: " + b);
+
+        double x6 = x3 - a * dx3;
+        double y6 = y3 + a * dy3;
+
+        double x7 = x2 - a * dx3;
+        double y7 = y2 + a * dy3;
+
+        drawSquare(x3, y3, x2, y2, x7, y7, x6, y6);
+        draw(x6, y6, x7, y7, (prevAlpha + alpha) - Math.PI/2, currentLevel + 1, levelToStop);
     }
 
-    public static void drawNextHouse(double x1, double y1, double x2, double y2, int currentLevel, int levelToStop) {
-        if (currentLevel >= levelToStop)
-            return;
+    private static void drawSquare(double x1, double y1, double x2, double y2, double x3, double y3, double x4,
+            double y4) {
+        // StdDraw.setPenColor(StdDraw.BLUE);
+        StdDraw.line(x1, y1, x2, y2);
+        // StdDraw.setPenColor(StdDraw.CYAN);
+        StdDraw.line(x2, y2, x3, y3);
+        // StdDraw.setPenColor(StdDraw.GREEN);
+        StdDraw.line(x3, y3, x4, y4);
+        // StdDraw.setPenColor(StdDraw.MAGENTA);
+        StdDraw.line(x4, y4, x1, y1);
+    }
 
-        double x_delta = x2 - x1;
-        double y_delta = y2 - y1;
-        double prevLineLength = Math.sqrt(Math.pow(x_delta, 2) + Math.pow(y_delta, 2));
-
-        // Calculate Triangle Coordinates
-        double alpha = Math.random() * Math.PI / 6 + Math.PI / 6;
-        System.out.println("prevline: " + prevLineLength);
-
-        double aLength = Math.sin(alpha) * prevLineLength;
-        double bLength = Math.cos(alpha) * prevLineLength;
-
-        double triangleTopX = x1 + Math.cos(alpha) * bLength;
-        double triangleTopY = y1 + Math.sin(alpha) * bLength;
-
-        
-        // Draw Triangle
-        StdDraw.line(x1, y1, triangleTopX, triangleTopY);
-        StdDraw.line(x2, y2, triangleTopX, triangleTopY);
-
-        // Calculate left Square Coordinates
-        double squareLowerLeftX = x1;
-        double squareLowerLeftY = y1;
-        double squareLowerRightX = triangleTopX;
-        double squareLowerRightY = triangleTopY;
-
-        double correctedAngle = Math.PI/2 - alpha;
-
-        double squareUpperLeftX = squareLowerLeftX - Math.cos(correctedAngle) * bLength;
-        double squareUpperLeftY = squareLowerLeftY + Math.sin(correctedAngle) * bLength;
-        double squareUpperRightX = squareLowerRightX - Math.cos(correctedAngle) * bLength;
-        double squareUpperRightY = squareLowerRightY + Math.sin(correctedAngle) * bLength;
-        
-        // Draw left Square
-        StdDraw.line(squareUpperLeftX, squareUpperLeftY, squareUpperRightX, squareUpperRightY);
-        StdDraw.line(squareUpperRightX, squareUpperRightY, squareLowerRightX, squareLowerRightY);
-        StdDraw.line(squareLowerRightX, squareLowerRightY, squareLowerLeftX, squareLowerLeftY);
-        StdDraw.line(squareLowerLeftX, squareLowerLeftY, squareUpperLeftX, squareUpperLeftY);
-        
-
-        // Calculate Square Coordinates
-        squareLowerLeftX = triangleTopX;
-        squareLowerLeftY = triangleTopY;
-        squareLowerRightX = x2;
-        squareLowerRightY = y2;
-
-        squareUpperLeftX = squareLowerLeftX + Math.cos(alpha) * aLength;
-        squareUpperLeftY = squareLowerLeftY + Math.sin(alpha) * aLength;
-        squareUpperRightX = squareLowerRightX + Math.cos(alpha) * aLength;
-        squareUpperRightY = squareLowerRightY + Math.sin(alpha) * aLength;
-
-        // Draw right Square
-        StdDraw.line(squareUpperLeftX, squareUpperLeftY, squareUpperRightX, squareUpperRightY);
-        StdDraw.line(squareUpperRightX, squareUpperRightY, squareLowerRightX, squareLowerRightY);
-        StdDraw.line(squareLowerRightX, squareLowerRightY, squareLowerLeftX, squareLowerLeftY);
-        StdDraw.line(squareLowerLeftX, squareLowerLeftY, squareUpperLeftX, squareUpperLeftY);
+    private static void drawTriangle(double x1, double y1, double x2, double y2, double x3, double y3) {
+        StdDraw.setPenColor(StdDraw.BLACK);
+        StdDraw.line(x1, y1, x2, y2);
+        StdDraw.line(x2, y2, x3, y3);
+        StdDraw.line(x3, y3, x1, y1);
     }
 }
