3a00022e4e2978ba304582e7463cf57972f4e41e
Ultrich-ea2 <118390217+Ultrich-ea2@users.noreply.github.com>
2023-01-01 22:34:11 +0100

    Update Aufgabe1.java

--- ultrich/uebungsblatt2/Aufgabe1.java
+++ ultrich/uebungsblatt2/Aufgabe1.java
@@ -63,7 +63,7 @@ public class Aufgabe1 {
 		// ob die Aussage richtig (true)
 		// oder falsch (false) ist.
 		boolean a = false; // Aussage A
-		boolean b = true; // Aussage B
+		boolean b = false; // Aussage B
 		boolean c = true; // Aussage C
 		boolean d = true; // Aussage D
 		boolean e = false; // Aussage E
