293612d553be0b97057071c7a70a19b15d24cf35
Marcel-Thiele <me@marcel-thiele.de>
2023-01-15 22:49:24 +0100

    did some work - not working tho

--- ultrich/uebungsblatt2/MazeSolver.java
+++ ultrich/uebungsblatt2/MazeSolver.java
@@ -2,6 +2,24 @@ import gdp.stdlib.StdDraw;
 
 public class MazeSolver {
 
+	public static void main(String[] args) {
+        int[][] maze = {
+            {1,1,1, 1, 1, 1, 2},
+            {0,1,0, 0, 1, 0, 0},
+            {0,1,0, 1, 1, 1, 1},
+            {1,1,1, 1, 0, 0, 0},
+            {0,0,1, 0, 1, 1, 0},
+            {0,1,1, 1, 1, 1, 1},
+            {3,1,0, 1, 1, 1, 1}
+            };
+
+        initDraw(maze);
+        draw(maze);
+        // if (solve(maze, 0, maze.length - 1))
+        //     drawFinish(maze);
+        solve(maze, 0, maze.length - 1);
+    }
+
 	public static boolean solve(int[][] maze, int row, int col) {
 		//int rowT,colT=0;
 		//boolean T=false;
@@ -9,10 +27,14 @@ public class MazeSolver {
 			
 			return true;
 		}
-		if(maze[row][col+1]==1&&maze[row+1][col]!=1) {
+		if(col-1 < 0) return false;
+		if(row+1 > maze.length) return false;
+
+		if(maze[row][col-1]==1&&maze[row+1][col]!=1) {
 			col+=1;
-			while(maze[row][col+1]==1&&maze[row+1][col]!=1){
-				//maze[row][col]=2;
+			while(maze[row][col-1]==1&&maze[row+1][col]!=1){
+				maze[row][col]=2;
+				draw(maze);
 				solve(maze,row,col++);
 				
 			}
@@ -23,10 +45,11 @@ public class MazeSolver {
 			colT=col+1;
 		}
 		*/
-		if(maze[row+1][col]==1&&maze[row][col+1]!=1) {
+		if(maze[row+1][col]==1&&maze[row][col-1]!=1) {
 			row+=1;
-			while(maze[row+1][col]==1&&maze[row][col+1]!=1) {
-				//maze[row][col]=2;
+			while(maze[row+1][col]==1&&maze[row][col-1]!=1) {
+				maze[row][col]=2;
+				draw(maze);
 				solve(maze,row++,col);
 			}
 		}
@@ -47,27 +70,38 @@ public class MazeSolver {
 	
 	
 
+
 	public static void draw(int[][] maze) {
-		
-		StdDraw.setXscale(0, 10); 
-		StdDraw.setYscale(0, 10);
-		
-		double x1=0,y1=0,x2=0,y2=10,x3=0,y3=0,x4=10,y4=0;
-		for(int i=0;i<=maze.length+1;i++) {
-			StdDraw.line(x1, y1, x2, y2);
-			StdDraw.line(x3, y3, x4, y4);
-			x1+=1;
-			x2+=1;
-			y3+=1;
-			y4+=1;
-		}
-		
-		
-		/*
-		Hier ist Platz für Ihre Lösung zum Anzeigen
-		*/
-	}
+        StdDraw.clear();
+        for (int x = 0; x < maze.length; x++) {
+            for (int y = 0; y < maze[0].length; y++) {
+                double drawX = (x + .5);
+                double drawY = maze.length - (y + .5);
+                if (maze[y][x] == 2) {
+                    StdDraw.setPenColor(StdDraw.GREEN);
+                    StdDraw.filledSquare(drawX, drawY, .5);
+                }
+                if (maze[y][x] == 3) {
+                    StdDraw.setPenColor(StdDraw.BLUE);
+                    StdDraw.filledSquare(drawX, drawY, .5);
+                }
+                if (maze[y][x] == 0) {
+                    StdDraw.setPenColor(StdDraw.GRAY);
+                    StdDraw.filledSquare(drawX, drawY, .5);
+                }
+                StdDraw.setPenColor(StdDraw.BLACK);
+                StdDraw.square(drawX, drawY, .5);
+            }
+        }
+
+        StdDraw.show(200);
+    }
 	
+	public static void initDraw(int[][] maze) {
+        StdDraw.setXscale(0, maze.length);
+        StdDraw.setYscale(0, maze[0].length);
+    }
+
 	/*
 	Hier können Sie bei Bedarf weitere Methoden oder Konstanten
 	unterbringen ...
