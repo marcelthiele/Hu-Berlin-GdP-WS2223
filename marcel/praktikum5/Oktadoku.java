e3efde22fd0e56b217a93b678ad61c921484efe6
Marcel-Thiele <me@marcel-thiele.de>
2023-01-20 11:06:58 +0100

    read and write works

--- marcel/praktikum5/Oktadoku.java
+++ marcel/praktikum5/Oktadoku.java
@@ -1,4 +1,4 @@
-package marcel.praktikum5;
+import gdp.stdlib.StdIn;
 
 public class Oktadoku {
     public enum Variante {
@@ -7,15 +7,38 @@ public class Oktadoku {
 
     private Variante v;
     // was sonst noch noetig ist …
+    char[][] puzzle;
 
     public Oktadoku(Variante var) {
         /* TODO */ }
 
     public void read() {
-        /* TODO */ }
+        puzzle = new char[8][8];
+        for (int x = 0; x < puzzle.length; x++) {
+            for (int y = 0; y < puzzle[0].length; y++) {
+                char ch = StdIn.readChar();
+                puzzle[x][y] = ch != '0' && ch != '.' ? ch : ' ';
+            }
+            StdIn.readChar();
+        }
+    }
 
     public void write() {
-        /* TODO */ }
+        for (int y = 0; y < puzzle[0].length; y++) {
+            if (y % 2 == 0) {
+                printLine();
+            }
+            for (int x = 0; x < puzzle.length; x++) {
+                if (x % 2 == 0) {
+                    System.out.print("| ");
+                }
+                System.out.print(puzzle[x][y] + " ");
+
+            }
+            System.out.print("|\n");
+        }
+        printLine();
+    }
 
     public boolean check() {
         /* TODO */
@@ -24,4 +47,10 @@ public class Oktadoku {
 
     public void solve() {
         /* TODO */ }
+
+    // -----------------------------------------
+
+    private void printLine() {
+        System.out.println("+-----+-----+-----+-----+");
+    }
 }
