6dbd841adb47f42438d1fa3b52447ac593bc6b24
Marcel-Thiele <me@marcel-thiele.de>
2023-01-20 10:18:30 +0100

    added files for praktikum5

---
+++ marcel/praktikum5/Oktadoku.java
@@ -0,0 +1,27 @@
+package marcel.praktikum5;
+
+public class Oktadoku {
+    public enum Variante {
+        normal, mitDiagonalen
+    };
+
+    private Variante v;
+    // was sonst noch noetig ist …
+
+    public Oktadoku(Variante var) {
+        /* TODO */ }
+
+    public void read() {
+        /* TODO */ }
+
+    public void write() {
+        /* TODO */ }
+
+    public boolean check() {
+        /* TODO */
+        return true;
+    }
+
+    public void solve() {
+        /* TODO */ }
+}
