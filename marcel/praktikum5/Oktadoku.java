4a47f4eb6eb42bfc208c6b5ba306e7ce96416b4d
Marcel-Thiele <me@marcel-thiele.de>
2023-01-20 12:20:27 +0100

    solve should be working

--- marcel/praktikum5/Oktadoku.java
+++ marcel/praktikum5/Oktadoku.java
@@ -30,15 +30,19 @@ public class Oktadoku {
     }
 
     public void write() {
-        for (int y = 0; y < puzzle[0].length; y++) {
+        writeSpecific(puzzle);
+    }
+
+    private void writeSpecific(char[][] currentPuzzle) {
+        for (int y = 0; y < currentPuzzle[0].length; y++) {
             if (y % NUMOFCELLSPERROWBLOCK == 0) {
                 System.out.println("+-----+-----+-----+-----+");
             }
-            for (int x = 0; x < puzzle.length; x++) {
+            for (int x = 0; x < currentPuzzle.length; x++) {
                 if (x % NUMOFCELLSPERCOLBLOCK == 0) {
                     System.out.print("| ");
                 }
-                System.out.print(puzzle[x][y] + " ");
+                System.out.print(currentPuzzle[x][y] + " ");
 
             }
             System.out.print("|\n");
@@ -51,23 +55,67 @@ public class Oktadoku {
     }
 
     public void solve() {
-            puzzle = solveRecursive(puzzle.clone());
+        puzzle = solveRecursive(puzzle.clone());
+        writeSpecific(puzzle);
     }
 
-    
     // -----------------------------------------
-    public char[][] solveRecursive(char[][] currentPuzzle){
-        // if(!check)
-        return puzzle;
+    public char[][] solveRecursive(char[][] currentPuzzle) {
+        if (!checkSpecificPuzzle(currentPuzzle)) {
+            // System.out.println("War nicht erfolgreich");
+            return null;
+        }
+        int[] emptyCell = getFirstEmptyCell(currentPuzzle);
+        int emptyCellCol = emptyCell[0];
+        int emptyCellRow = emptyCell[1];
+        if(emptyCellCol == -1 && emptyCellRow == -1) return currentPuzzle;
+
+        // System.out.println("found empty cell");
+        for (int currentNumberToCheck = 1; currentNumberToCheck < 9; currentNumberToCheck++) {
+            char[][] clonedPuzzle = copyArray(currentPuzzle);
+            clonedPuzzle[emptyCellCol][emptyCellRow] = (char) (currentNumberToCheck + 48);
+            // writeSpecific(clonedPuzzle);
+            char[][] solvedPuzzle = solveRecursive(clonedPuzzle);
+            if (solvedPuzzle != null)
+                return solvedPuzzle;
+
+        }
+
+        return null;
     }
 
-    public boolean checkSpecificPuzzle(char[][] currentPuzzle){
+    public int[] getFirstEmptyCell(char[][] currentPuzzle){
+        for (int row = 0; row < currentPuzzle[0].length; row++) {
+            for (int col = 0; col < currentPuzzle.length; col++) {
+                if (currentPuzzle[col][row] == ' ') {
+                    return new int[]{col, row};
+                }
+            }
+        }
+
+        return new int[]{-1, -1};
+    }
+
+    public char[][] copyArray(char[][] arrayToCopy){
+        char[][] retArray = new char[arrayToCopy.length][arrayToCopy[0].length];
+
+        for(int i = 0; i < retArray.length; i++){
+            for(int j = 0; j < retArray[0].length; j++){
+                retArray[i][j] = arrayToCopy[i][j];
+            }
+        }
+
+        return retArray;
+    }
+
+    public boolean checkSpecificPuzzle(char[][] currentPuzzle) {
         boolean retCheck = checkRows(currentPuzzle) && checkColumns(currentPuzzle) && checkBlocks(currentPuzzle);
-        if(this.v == Variante.mitDiagonalen) retCheck = retCheck && checkDiagonals(currentPuzzle);
+        if (this.v == Variante.mitDiagonalen)
+            retCheck = retCheck && checkDiagonals(currentPuzzle);
         return retCheck;
     }
 
-    public boolean checkRows(char[][] currentPuzzle ) {
+    public boolean checkRows(char[][] currentPuzzle) {
         for (int row = 0; row < currentPuzzle[0].length; row++) {
             BitSet numberWasInRow = new BitSet(); // TODO hier einen besseren namen
             for (int col = 0; col < currentPuzzle.length; col++) {
@@ -80,7 +128,7 @@ public class Oktadoku {
         return true;
     }
 
-    public boolean checkColumns(char[][] currentPuzzle ) {
+    public boolean checkColumns(char[][] currentPuzzle) {
         for (int col = 0; col < currentPuzzle.length; col++) {
             BitSet numberWasInCol = new BitSet(); // TODO hier einen besseren namen
             for (int row = 0; row < currentPuzzle[0].length; row++) {
@@ -93,7 +141,7 @@ public class Oktadoku {
         return true;
     }
 
-    public boolean checkBlocks(char[][] currentPuzzle ) {
+    public boolean checkBlocks(char[][] currentPuzzle) {
         for (int rowBlockIndex = 0; rowBlockIndex < currentPuzzle[0].length / NUMOFCELLSPERROWBLOCK; rowBlockIndex++) {
             for (int colBlockIndex = 0; colBlockIndex < currentPuzzle.length / NUMOFCELLSPERCOLBLOCK; colBlockIndex++) {
                 if (!checkBlock(currentPuzzle, rowBlockIndex, colBlockIndex))
@@ -119,7 +167,7 @@ public class Oktadoku {
         return true;
     }
 
-    private boolean checkDiagonals(char[][] currentPuzzle ) {
+    private boolean checkDiagonals(char[][] currentPuzzle) {
         return false;
     }
 
